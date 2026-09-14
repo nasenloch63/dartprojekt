d39d0289c1de4425368d3045e3ac9e91b5b35b6d
nasenloch63 <yasin.aissani@gmx.de>
2026-09-14 15:13:49 +0200

    update arbeit

--- selbst.dart/greeter.dart
+++ selbst.dart/greeter.dart
@@ -2,12 +2,32 @@ import 'dart:io';
 
 void main() {
   String? vorname;
-  
-  print("Vorname?");
-  vorname = stdin.readLineSync();
+  String? nachname;
+  int? age;
+  String? sex;
 
+  print("Vorname, Nachname, Alter, M/F?");
+  vorname = stdin.readLineSync();
   while (vorname == null || vorname.isEmpty) {
-  print("Fehler!"); 
+  print("Fehler! Bitte Vorname erneut eingeben: Max, Mustermann, 21, M");
+  vorname = stdin.readLineSync(); 
   }
+
+  print("Vorname, Nachname, Alter, M/F?");
+  nachname = stdin.readLineSync();
+  while (nachname == null || nachname.isEmpty) {
+  print("Fehler! Bitte Vorname erneut eingeben: Max, Mustermann, 21, M");
+  nachname = stdin.readLineSync(); 
+  }
+
+  print("Vorname, Nachname, Alter, M/F?");
+  age = int.tryParse();
+  while (age == null) {
+  print("Fehler! Bitte Vorname erneut eingeben: Max, Mustermann, 21, M");
+  age = int.tryParse(); 
+  }
+
+
+
   print("Ich grüße dich $vorname!");
 }
