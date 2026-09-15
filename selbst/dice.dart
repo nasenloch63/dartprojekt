67d15bcd3cce2b05178e82406c1e333cde8caf68
nasenloch63 <yasin.aissani@gmx.de>
2026-09-15 10:06:59 +0200

    changes

---
+++ selbst/dice.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+
+
+int? = liste
+
+void main() {
+  var rng = Random();
+  for (var i = 0; i < 10; i++) {
+    print(rng.nextInt(6));
+  }
+
+  
+}
--- selbst/greeter.dart
+++ selbst/greeter.dart
@@ -3,31 +3,35 @@ import 'dart:io';
 void main() {
   String? vorname;
   String? nachname;
+  String? alterEingabe;
   int? age;
   String? sex;
 
+// abfrage nach Vorname
   print("Vorname, Nachname, Alter, M/F?");
   vorname = stdin.readLineSync();
   while (vorname == null || vorname.isEmpty) {
   print("Fehler! Bitte Vorname erneut eingeben: Max, Mustermann, 21, M");
   vorname = stdin.readLineSync(); 
   }
-
+// abfrage nach Nachname
   print("Vorname, Nachname, Alter, M/F?");
   nachname = stdin.readLineSync();
   while (nachname == null || nachname.isEmpty) {
   print("Fehler! Bitte Vorname erneut eingeben: Max, Mustermann, 21, M");
   nachname = stdin.readLineSync(); 
   }
-
+// abfrage nach Alter
+  
+  if (alterEingabe != null) {
+    age = int.tryParse(alterEingabe);
+  }
   print("Vorname, Nachname, Alter, M/F?");
-  age = int.tryParse();
-  while (age == null) {
+  while (alterEingabe == null || alterEingabe.isEmpty) {
   print("Fehler! Bitte Vorname erneut eingeben: Max, Mustermann, 21, M");
-  age = int.tryParse(); 
+  age = int.tryParse(alterEingabe ?? '');
   }
 
-
-
+//ausgabe greeter mit Vor Nachname Alter und Age
   print("Ich grüße dich $vorname!");
 }
