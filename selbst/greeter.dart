605a8f17c7115c4614cf37e2ee260dac1ab8587f
nasenloch63 <yasin.aissani@gmx.de>
2026-09-15 15:06:11 +0200

    greeter fast fertig ENUM

--- selbst/greeter.dart
+++ selbst/greeter.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 enum Geschlecht {
   M,
-  W
+  F
 }
 void main() {
   String? vorname;   // texteingabe vorname
@@ -23,7 +23,7 @@ void main() {
   print("Vorname, Nachname, Alter, M/F?");
   nachname = stdin.readLineSync();
   while (nachname == null || nachname.isEmpty) {
-  print("Fehler! Bitte Vorname erneut eingeben: Max, Mustermann, 21, M");
+  print("Fehler! Bitte Namen erneut eingeben: Max, Mustermann, 21, M");
   nachname = stdin.readLineSync(); 
   }
 // abfrage nach Alter
@@ -35,13 +35,22 @@ void main() {
     age = int.tryParse(alterEingabe);
   }
   while (age == null || age < 0 || age > 150) {
-  print("Fehler! Bitte Vorname erneut eingeben: Max, Mustermann, 21, M");
+  print("Fehler! Bitte Alter erneut eingeben: Max, Mustermann, 21, M");
   alterEingabe = stdin.readLineSync();
   age = int.tryParse(alterEingabe ?? '');
   }
   // frage nach geschlecht 
   print("Vorname, Nachname, Alter, M/F?");
   geschlechtEingabe = stdin.readLineSync(); 
+  if (geschlechtEingabe == "M") {
+    sex = Geschlecht.M;
+  }
+  else if (geschlechtEingabe == "F") {
+    sex = Geschlecht.F;
+  }
+  while (sex == null) {
+    print("M/F");
+  }
 //ausgabe greeter mit Vor Nachname Alter und Age
   print("Ich grüße dich $vorname!");
 }
